e342568a8708a3e829f81dd4fc8aa029ac3fd410
Christian Berendt <berendt@b1-systems.de>
2015-01-08 10:00:03 +0100

    Add Packer template from b1-systems/packer-templates
    This packer template is usded to create the packstack-vagrant
    basebox and should be part of packstack-vagrant.
    
    Change-Id: I8283faaeafb12b9c8be7c85b3fc9ee093cbf154e

---
+++ packer/scripts/cleanup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf /dev/.udev/
+rm -rf /tmp/*
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+sync
---
+++ packer/scripts/sshd.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
